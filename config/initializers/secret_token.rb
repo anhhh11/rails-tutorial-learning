abe5ccbbc9b1c47443cd805775cd3423d47f55d1
anhhhh.11@gmail.com <anhhhh.11@gmail.com>
2014-06-06 15:52:35 +0700

    template generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestRails::Application.config.secret_key_base = '514d412f2095ace7004d18485021d4e36894012b0afec394cd10c80be3a218ab04da4fa696c86e2af8cac531a23d019aefed8bff05e35d9a0c2cb663135f13ee'
